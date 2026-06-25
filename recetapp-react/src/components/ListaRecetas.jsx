24cb91a19c7a22397f354e10798dd5a94e91e09e
Christopher cortés <Christopher.cortes15@inacapmail.cl>
2026-06-25 15:21:53 -0400

    Fase 3: Lista de recetas

--- recetapp-react/src/components/ListaRecetas.jsx
+++ recetapp-react/src/components/ListaRecetas.jsx
@@ -1,17 +1,26 @@
 import RecetaCard from './RecetaCard'
+import PropTypes from 'prop-types'
 
 function ListaRecetas({ recetas }) {
-  if (recetas.length === 0) {
+  if (!Array.isArray(recetas) || recetas.length === 0) {
     return <p>No se encontraron recetas para esa categoría.</p>
   }
 
   return (
     <section className="lista-recetas">
       {recetas.map((receta) => (
-        <RecetaCard key={receta.id} receta={receta} />
+        <RecetaCard key={receta.id} {...receta} />
       ))}
     </section>
   )
+}
+
+ListaRecetas.propTypes = {
+  recetas: PropTypes.arrayOf(PropTypes.object),
+}
+
+ListaRecetas.defaultProps = {
+  recetas: [],
 }
 
 export default ListaRecetas
